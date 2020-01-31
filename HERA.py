a45b31d676ac4ea843bf6bdfc4dab30c04bd6a00
hadams25 <46791347+hadams25@users.noreply.github.com>
2020-01-30 18:17:38 -0600

    Remove unneeded comments

--- HERA.py
+++ HERA.py
@@ -84,8 +84,6 @@ async def on_message(message):
             await channel.send(client.get_user(uuid).name + " has been dad'ed " + str(author_stats) + " times.")
 
     try:
-        #I’m
-        # this if statement causes an error for some reason
         if msgStartsWith(message, "im ") or msgStartsWith(message, "me ") or ( ord(message.content.lower()[0]) == 105 and (ord(message.content.lower()[1]) == 39 or ord(message.content.lower()[1]) == 8217) and ord(message.content.lower()[2]) == 109 and message.content.lower()[3] == " "):
             if message.author.id in noreply: return
             msg = message.content
